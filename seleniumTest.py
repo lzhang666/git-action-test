b8113d56f2fe1e95c1a4b7806320b7f7e54d5d3d
Li Zhang <lizhang@Lis-MacBook-Pro.local>
2020-10-02 17:09:46 -0500

    try selenium 5

--- seleniumTest.py
+++ seleniumTest.py
@@ -1,9 +1,13 @@
 from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 
 path = '/home/runner/work/git-action-test/git-action-test/chromedriver'
-driver = Chrome(path)
+options = Options()
+options.headless = True
+driver = Chrome(executable_path=path, options=options)
 
 driver.get('https://www.uchicago.edu/')
+driver.set_window_size(1920, 1080)
 print(driver.title)
 driver.quit()
 
